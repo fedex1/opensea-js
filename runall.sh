ec60af09179808cee578d25e67d9c2286fc97a71
ralph yozzo <ralph@brooklynmarathon.com>
2023-12-21 20:43:27 -0500

    ralph-desktop

---
+++ runall.sh
@@ -0,0 +1,4 @@
+#!
+curl --request GET \
+     --url https://testnets-api.opensea.io/api/v2/chain/sepolia/account/0xa9ed1cb1801e0951bb7ae83aa6f6a31a17794441/nfts \
+     --header 'accept: application/json'
